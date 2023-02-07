553be13743ddb5a291f5dcd538312ee17e6dfdd6
Rust62 <rustamgazanfar@gmail.com>
2023-02-06 21:15:22 -0800

    the file conftest modified

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,21 +2,21 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(params=["chrome"], scope='class')
+@pytest.fixture(params=["chrome", "firefox", "edge"], scope='class')
 def init_driver(request):
     global driver
     if request.param == "chrome":
         options = webdriver.Chrome()
         options.headless = False
         driver = webdriver.Chrome()
-    # elif request.param == "firefox":
-    #     options = webdriver.Firefox()
-    #     options.headless = False
-    #     driver = webdriver.Firefox()
-    # elif request.param == "edge":
-    #     options = webdriver.Edge()
-    #     options.headless = False
-    #     driver = webdriver.Edge()
+    elif request.param == "firefox":
+        options = webdriver.Firefox()
+        options.headless = False
+        driver = webdriver.Firefox()
+    elif request.param == "edge":
+        options = webdriver.Edge()
+        options.headless = False
+        driver = webdriver.Edge()
     request.cls.driver = driver
     driver.maximize_window()
     yield
